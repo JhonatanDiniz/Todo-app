896c64247290b7fc9a17711b5e290e153d7b8070
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-10-29 22:06:06 -0300

    Criado projeto e iniciado componente Header

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="bg-zinc-900 text-zinc-200 h-screen w-full">
+      <Header/>
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header(){
+  return(
+    <header className="bg-zinc-800 flex items-center justify-between p-4">
+      <div>
+        <h1 className="text-xl font-semibold">Todo-App</h1>
+      </div>
+      <div>
+        <h1>Sign-in</h1>
+      </div>
+    </header>
+  )
+}
