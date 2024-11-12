2adb676b12d9773eec7fa5359aba3b9d0ac6ebe2
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-11-12 08:30:02 -0300

    Incluido funcionalidade de sair da aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "./ui/button"
+
 export default function Header(){
+  const navigate = useNavigate()
+  async function sair() {
+    sessionStorage.removeItem('Token')
+    navigate('/signin') 
+  }
+
   return(
     <header className="bg-zinc-800 flex items-center justify-between p-4">
       <div>
         <h1 className="text-xl font-semibold">Todo-App</h1>
       </div>
       <div>
-        <h1>Sign-in</h1>
+        <Button onClick={sair}>Sair</Button>
       </div>
     </header>
   )
