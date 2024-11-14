ab5a84049dd427aaf7c907fc7997710e2f076a59
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-11-13 22:47:39 -0300

    Exibindo mensagem para novos usuários que não possuem tarefas

--- src/pages/app/Tasks.tsx
+++ src/pages/app/Tasks.tsx
@@ -2,10 +2,13 @@ import FormNewTask from "@/components/FormNewTask";
 import ModalDialog from "@/components/ModalDialog";
 import TableTask from "@/components/TableTask";
 import { Button } from "@/components/ui/button";
+import { TaskContext } from "@/context/TaskContext";
 import { Plus } from "lucide-react";
+import { useContext } from "react";
 
 
 export default function Tasks() {
+  const { tasks } = useContext(TaskContext)
   return (
       <div className="container mx-auto mt-12">
         <div className="flex items-center justify-end">
@@ -21,7 +24,7 @@ export default function Tasks() {
           />
         </div>
         <main className="container mx-auto mt-12">
-          <TableTask/>
+          {tasks.length > 0 ? <TableTask/> : <h1 className="text-2xl text-center font-bold">Você ainda não possui tarefas!</h1>}
         </main>
       </div>
   )
